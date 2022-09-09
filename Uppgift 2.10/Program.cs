d49cf96cd1993e47719b03c545cf6e751e3b98c5
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-09 14:26:57 +0200

    I denna uppgift har jag gjort så att programmet ställer en fråga, användaren svarar och jag svarar tillbaka med att säga "Din favorit bok är" och sen kommer texten användaren skriver.

--- Uppgift 2.10/Program.cs
+++ Uppgift 2.10/Program.cs
@@ -8,6 +8,7 @@ namespace Uppgift_2_10
         {
             Console.WriteLine("Vilken bok är din favorit?");
             string x = Console.ReadLine();
+            Console.WriteLine("Din favorit bok är "+x);
         }
     }
 }
