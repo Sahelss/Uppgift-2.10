ee0126ca776e0393cac6c13763a45901765b68d0
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-09 14:21:38 +0200

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken bok är din favorit?");
+            string x = Console.ReadLine();
+        }
+    }
+}
+    
